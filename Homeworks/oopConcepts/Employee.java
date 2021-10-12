b14539abe25bea6120e8605c92d74d63767b553f
Vanessa Moreno Torres <87245201+VanessaMorenoTorres@users.noreply.github.com>
2021-10-11 22:49:21 -0500

    initial

---
+++ Homeworks/oopConcepts/Employee.java
@@ -0,0 +1,28 @@
+package oopConcepts;
+
+public class Employee extends Person{
+	double hourlyRate;
+	String healthIns;
+	
+	public Employee(String firstName, String lastName, int age, double hourlyRate, String healthIns) {
+		super(firstName, lastName, age);
+		this.hourlyRate = hourlyRate;
+		this.healthIns = healthIns;
+	}
+	
+	
+	public Employee( double hourlyRate, String healthIns) {
+		this.hourlyRate = hourlyRate;
+		this.healthIns = healthIns;
+	}
+
+
+	public Employee() {
+	}
+
+
+	void display(){
+		System.out.println("Hourly rate: "+ this.hourlyRate);
+		System.out.println("Has health insurance: "+ this.healthIns);
+	}
+}
